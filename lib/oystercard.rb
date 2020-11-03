12fbeeefc616f2c7b673091123548d2764f299ec
Melissa <mystic101@gmail.com>
2020-11-03 15:44:38 +0000

    Allowed travel if min balance met

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,10 +1,12 @@
 class OysterCard
 
+MIN_BALANCE = 1
 MAX_BALANCE = 90
-attr_reader :balance
+attr_reader :balance, :journey
 
   def initialize
     @balance = 0
+    @journey = false
   end
 
   def top_up(amount)
@@ -15,4 +17,26 @@ attr_reader :balance
   def deduct(amount)
     @balance -= amount
   end
+
+  def in_journey?
+    @journey == true
+#    if @journey == true
+#      return true
+# => else, return false
+
+  end
+
+  def touch_in
+    raise "Not enough funds" if @balance < MIN_BALANCE
+    @journey = true
+  end
+
+  def touch_out
+    @journey = false
+  end
+
 end
+
+#expect card to respond to touch_in, touch_out
+#when touch_in, in_journey? returns true until touch_out
+#when touch_out, in_journey? return false
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -9,6 +9,10 @@ describe OysterCard do
     expect(subject.balance).to eq 0
   end
 
+  it "initialized card is not on a journey" do
+    expect(subject.journey).to eq false
+  end
+
   describe "#top_up" do
     it "allows the user to add money" do
       subject.top_up(5)
@@ -23,10 +27,35 @@ describe OysterCard do
 
   describe "#deduct" do
     it "reduces the oyster card balance" do
-      card = OysterCard.new
-      card.top_up(10)
-      card.deduct(5)
-      expect(card.balance).to eq 5
+      subject.top_up(10)
+      subject.deduct(5)
+      expect(subject.balance).to eq 5
+    end
+  end
+
+  describe "#touch_in" do
+    it "responds to #touch_in" do
+      expect(subject).to respond_to :touch_in
+    end
+    it "when touch_in, the journey has started" do
+      subject.top_up(OysterCard::MIN_BALANCE)
+      subject.touch_in
+      expect(subject.in_journey?).to eq true
+    end
+    it "checks that there is the min amount needed" do
+      subject.top_up(OysterCard::MIN_BALANCE)
+      subject.deduct(0.50)
+      expect { subject.touch_in }.to raise_error(RuntimeError, "Not enough funds")
+    end
+  end
+
+  describe "#touch_out" do
+    it "responds to #touch_out}" do
+      expect(subject).to respond_to :touch_out
+    end
+    it "when touch_out, the journey will have ended" do
+      subject.touch_out
+      expect(subject.in_journey?).to eq false
     end
   end
 end
